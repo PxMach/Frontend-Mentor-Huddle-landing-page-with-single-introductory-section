e0d6c19efbd39f92f372ab26832f7b82eaabf44b
PxMach <moundjidmachghour58@gmail.com>
2025-07-08 09:51:54 +0100

    feat: enhance layout and styling in Main component; add responsive design and hover effects

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import styles from "./header.module.css";
 
 export default function Header() {
   return (
-    <header className={` ${styles.header}`}>
+    <header className={` ${styles.header} `}>
       <img src={logo} alt="logo Huddle" className="w-[150px]" />
     </header>
   );
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,26 +1,42 @@
+import React from "react";
 import styles from "./main.module.css";
 import mockups from "../../assets/images/illustration-mockups.svg";
 
 export default function Main() {
+  const [isHovered, setIsHovered] = React.useState(false);
+
   return (
-    <main className={`${styles.main}  mt-4 flex flex-col items-center `}>
+    <main
+      className={`${styles.main}  mt-15 flex flex-col items-center min-[500px]:flex-row min-[500px]:gap-15 min-[500px]:items-start min-[500px]:w-[77rem]`}
+    >
       <img className="mb-8" src={mockups} alt="illustration mockups" />
-      <h1 className="text-[28px] text-white text-center font-semibold mb-4">
-        Build The Community Your Fans Will Love
-      </h1>
-      <p className="text-gray-100 text-center">
-        Huddle re-imagines the way we build communities. You have a voice, but
-        so does your audience. Create connections with your users as you engage
-        in genuine discussion.
-      </p>
-      <button
-        className="bg-white border-none rounded-3xl text-violet-500 py-2 mt-6 w-[200px] shadow-sm shadow-black  "
-        type="button"
-      >
-        Register
-      </button>
-      <div>
-        {/* icon */}
+      <div className="flex flex-col items-center min-[500px]:items-start min-[500px]:mt-12 ">
+        <h1 className="text-[25px] text-white text-center font-semibold mb-4 min-[500px]:text-[38px] min-[500px]:text-left">
+          Build The Community Your Fans Will Love
+        </h1>
+        <p className="text-gray-300 text-center min-[500px]:text-left">
+          Huddle re-imagines the way we build communities. You have a voice, but
+          so does your audience. Create connections with your users as you
+          engage in genuine discussion.
+        </p>
+        <button
+          style={{
+            color: isHovered ? "white" : "var(--Soft_Magenta)",
+            backgroundColor: isHovered ? "var(--Soft_Magenta)" : "white",
+            transition: "background 0.2s",
+          }}
+          className="bg-white border-none rounded-3xl py-2 mt-6 w-[200px] shadow-sm shadow-black hover:cursor-pointer "
+          type="button"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          Register
+        </button>
+      </div>
+      <div className="mt-8 flex gap-3 min-[500px]:absolute min-[500px]:right-15 min-[500px]:bottom-12">
+        <i class="devicon-facebook-plain text-white p-3 border-[1px] rounded-[50%] hover:text-[violet]"></i>
+        <i class="devicon-twitter-original text-white p-3 border-[1px] rounded-[50%] hover:text-[violet]"></i>
+        <i class="fa-brands fa-instagram text-white p-3 border-[1px] rounded-[50%] hover:text-[violet]"></i>
       </div>
     </main>
   );
